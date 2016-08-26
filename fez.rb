76c0c6ebd9b9d6064824470ad31209295695cfc5
Jeremy Woertink <jeremywoertink@gmail.com>
2016-08-26 16:46:41 -0700

    update formula to 0.4.1

--- fez.rb
+++ fez.rb
@@ -1,9 +1,9 @@
 class Fez < Formula
   desc "fez is a Kemal app builder"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/archive/v0.4.0.tar.gz"
-  version "0.4.0"
-  sha256 "60392a832a9bf4b02bc840697fc0acf7d6afd2c77065aad4ef2f9d23b9cf66ac"
+  url "https://github.com/jwoertink/fez/archive/v0.4.1.tar.gz"
+  version "0.4.1"
+  sha256 "68f76b8e1c543ea4e1b64afe478c4417179c36b03e32711dcea1aba8d0226f35"
   revision 1
 
   def install
