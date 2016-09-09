294dc085fe363267e95f712bad9983cfa6be1b4a
Jeremy Woertink <jeremywoertink@gmail.com>
2016-09-09 16:05:17 -0700

    new release

--- fez.rb
+++ fez.rb
@@ -1,9 +1,9 @@
 class Fez < Formula
   desc "fez is a Kemal app builder"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/archive/v0.4.1.tar.gz"
-  version "0.4.1"
-  sha256 "68f76b8e1c543ea4e1b64afe478c4417179c36b03e32711dcea1aba8d0226f35"
+  url "https://github.com/jwoertink/fez/archive/v0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "02d6304cfb83dcdc8888045e74893ab5f29940c378e7b842d7c47f699cd5bc1b"
   revision 1
 
   def install
