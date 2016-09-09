983cc8c4bcd8284c95bf3c5cd253be916d1f9f1b
Jeremy Woertink <jeremywoertink@gmail.com>
2016-09-09 16:48:09 -0700

    require crystal to install fez

--- fez.rb
+++ fez.rb
@@ -5,6 +5,7 @@ class Fez < Formula
   version "0.5.0"
   sha256 "02d6304cfb83dcdc8888045e74893ab5f29940c378e7b842d7c47f699cd5bc1b"
   revision 1
+  depends_on "crystal-lang"
 
   def install
     system "make"
