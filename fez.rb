97916150274a35be0f4d3070223c6bf14e2ffb45
Jeremy Woertink <jeremywoertink@gmail.com>
2016-10-12 17:10:37 -0700

    bumped to 0.6.3

--- fez.rb
+++ fez.rb
@@ -1,9 +1,9 @@
 class Fez < Formula
   desc "fez is a Kemal app builder"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/archive/v0.6.2.tar.gz"
-  version "0.6.2"
-  sha256 "34e23654ed5cc7e0d0096af9e604637c93b4a19f07a7529ebd5bfb6997a1194a"
+  url "https://github.com/jwoertink/fez/archive/v0.6.3.tar.gz"
+  version "0.6.3"
+  sha256 "9fa4747e04852b77d85b5581aa4e99e699d2eb75117ceb68c536c5cbc6ecb27d"
   revision 1
   depends_on "crystal-lang"
 
