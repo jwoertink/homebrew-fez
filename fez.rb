da7129ed12ec49855baa4485fe6aaa6886556c8f
Jeremy Woertink <jeremywoertink@gmail.com>
2017-03-21 14:47:32 -0700

    v0.9.1

--- fez.rb
+++ fez.rb
@@ -1,14 +1,13 @@
 class Fez < Formula
-  desc "fez is a Kemal app builder"
+  desc "fez is scaffold generator for Kemal and more"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/archive/v0.9.0.tar.gz"
-  version "0.9.0"
-  sha256 "7247a839154ade7da09e62e5bd753d7e8e15d78c71bf3aa4e1b1b3dae0d0877a"
+  url "https://github.com/jwoertink/fez/archive/v0.9.1.tar.gz"
+  version "0.9.1"
+  sha256 "29e91d25d11c08537918fa0da66fbab7f65feae4648411c713310ece980227eb"
   revision 1
   depends_on "crystal-lang"
 
   def install
-    system "shards", "install"
     system "make"
     bin.install "bin/fez"
   end
