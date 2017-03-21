eec4aa0bcc45b3e5c94e621b1d0328a7ba0dea0d
Jeremy Woertink <jeremywoertink@gmail.com>
2017-03-21 15:41:04 -0700

    downloading fez from the raw binary instead of building during installation

--- fez.rb
+++ fez.rb
@@ -1,14 +1,17 @@
 class Fez < Formula
   desc "fez is scaffold generator for Kemal and more"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/archive/v0.9.1.tar.gz"
+  url "https://github.com/jwoertink/fez/releases/download/v0.9.1/fez"
   version "0.9.1"
-  sha256 "29e91d25d11c08537918fa0da66fbab7f65feae4648411c713310ece980227eb"
+  sha256 "0a6e277aa8d327f0f3b9b5c64ef2e79a613f28ec7b07cc907a2908f261e69667"
   revision 1
   depends_on "crystal-lang"
 
   def install
-    system "make"
-    bin.install "bin/fez"
+    bin.install buildpath/"fez"
+  end
+
+  test do
+    system "fez -v"
   end
 end
