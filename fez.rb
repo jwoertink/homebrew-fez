23faa6a77cdad2e46b704bfc906cbac9280bce9f
Jeremy Woertink <jeremywoertink@gmail.com>
2017-05-20 09:39:41 -0700

    update to 0.10.0

--- fez.rb
+++ fez.rb
@@ -1,9 +1,9 @@
 class Fez < Formula
   desc "fez is scaffold generator for Kemal and more"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/releases/download/v0.9.1/fez"
-  version "0.9.1"
-  sha256 "0a6e277aa8d327f0f3b9b5c64ef2e79a613f28ec7b07cc907a2908f261e69667"
+  url "https://github.com/jwoertink/fez/releases/download/v0.10.0/fez"
+  version "0.10.0"
+  sha256 "1f72b102adb17ef0800e745b4aaa6cd0a2844d412673862ba4dfeee77bb55610"
   revision 1
   depends_on "crystal-lang"
 
