452cd140d076c33ea231d6905ff09a802c6472ec
Jeremy Woertink <jeremywoertink@gmail.com>
2017-11-24 19:12:33 -0800

    updated incorrect url. fixes #3

--- fez.rb
+++ fez.rb
@@ -1,7 +1,7 @@
 class Fez < Formula
   desc "fez is scaffold generator for Kemal and more"
   homepage "http://github.com/jwoertink/fez"
-  url "https://github.com/jwoertink/fez/releases/download/v0.10.0/fez"
+  url "https://github.com/jwoertink/fez/archive/0.10.0.tar.gz"
   version "0.10.0"
   sha256 "1f72b102adb17ef0800e745b4aaa6cd0a2844d412673862ba4dfeee77bb55610"
   revision 1
